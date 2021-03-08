bcc51b3e7edbe06f2b49d816ff9ff40ed3bee76b
Your Name <you@example.com>
2021-03-08 14:17:23 +0100

    basic gitignore, basic installer~~~bkifft

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt-distupgrade
+sudo apt install python3-dev python3-flask python3-smbus python3-picamera
+sudo ln -sf "`pwd`/LINUX_ROOT/opt/controllbox" /opt/controllbox
+sudo ln -sf "`pwd`/LINUX_ROOT/etc/systemd/system/controllbox.service" /etc/systemd/system/controllbox.service
+sudo systemctl daemon-reload
+sudo systemctl enable controllbox
+sudo systemctl start controllbox
+
+
+
